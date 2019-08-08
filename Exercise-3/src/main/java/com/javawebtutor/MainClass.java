3a82bd342e1c03985ade6fbc763ea8e8ac5db1ec
rajkadhi10 <raj.kadhi10@gmail.com>
2019-08-08 19:13:38 +0530

    Exercise-3

---
+++ Exercise-3/src/main/java/com/javawebtutor/MainClass.java
@@ -0,0 +1,60 @@
+package com.javawebtutor;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class MainClass {
+
+    private static EntityManager em;
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("HelloWorld");
+        em = emf.createEntityManager();
+        Message msg;
+        Scanner input = new Scanner(System.in);
+        String c = "y";
+        do {
+            System.out.println("1. Enter  2. Retrive");
+            int n = input.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Enter Employee Name");
+                    String name = input.next();
+                    msg = new Message(name);
+                    em.getTransaction().begin();
+                    em.persist(msg);
+                    em.getTransaction().commit();
+                    break;
+                case 2:
+                    try {
+                        em.getTransaction().begin();
+                        Query query = em.createQuery("SELECT e FROM Message e");
+                        List lst = query.getResultList();
+                        Iterator it = lst.iterator();
+                        while (it.hasNext()) {
+                            Message msg1;
+                            msg1 = (Message) it.next();
+                            System.out.println("Id:" + msg1.getId());
+                            System.out.println(" Message:" + msg1.getName());
+                        }
+                        em.getTransaction().commit();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+            }
+
+            System.out.println("Continue?");
+            c = input.next();
+        } while (!"n".equals(c));
+
+    }
+
+}
