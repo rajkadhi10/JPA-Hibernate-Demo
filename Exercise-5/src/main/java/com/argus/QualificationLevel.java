e6c533eb3e4ac5d37f9db3538b257357eb03a270
rajkadhi10 <raj.kadhi10@gmail.com>
2019-08-10 15:45:57 +0530

    Exercise-5

---
+++ Exercise-5/src/main/java/com/argus/QualificationLevel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.argus;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author raj
+ */
+
+public class QualificationLevel {
+
+    public enum q_level
+    {
+        Beginner,
+        Intermediate,
+        Pro
+    }
+
+    
+    
+
+}
